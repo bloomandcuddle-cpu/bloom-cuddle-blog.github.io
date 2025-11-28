64d932c656e952bff04842449116cfc29f1ed353
haneen work <bloomandcuddle@gmail.com>
2025-11-28 19:13:01 +0300

    Create reports-list.js

---
+++ scripts/reports-list.js
@@ -0,0 +1,6 @@
+{
+  name: "eczema",
+  title: "Baby Eczema", 
+  cover: "reports-images/eczema.jpg",
+  description: "Simple steps to identify eczema, care for flare-ups, and choose the right products."
+}
