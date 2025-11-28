88c6fcde5b9378f8455e9441412d7cc05ea1d931
haneen work <bloomandcuddle@gmail.com>
2025-11-28 20:35:33 +0300

    Update reports-list.js

--- scripts/reports-list.js
+++ scripts/reports-list.js
@@ -1,6 +1,6 @@
 {
   name: "eczema",
   title: "Baby Eczema", 
-  cover: "reports-images/eczema.jpg",
+  cover: "images/eczema-baby.jpg",
   description: "Simple steps to identify eczema, care for flare-ups, and choose the right products."
 }
