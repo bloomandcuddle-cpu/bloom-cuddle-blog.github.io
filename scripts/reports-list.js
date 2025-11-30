5c16d585596b3f55e57ce757e9163ea93c474ef9
haneen work <bloomandcuddle@gmail.com>
2025-11-30 11:35:29 +0300

    Update reports-list.js

--- scripts/reports-list.js
+++ scripts/reports-list.js
@@ -5,5 +5,11 @@ const reports = [
     title: "Baby Eczema", 
     cover: "images/eczema-baby.jpg",
     description: "Simple steps to identify eczema, care for flare-ups, and choose the right products."
+  },
+  {
+    name: "Anti-Colic Bottle",
+    title: "Top 5 Anti-Colic Bottles", 
+    cover: "images/Anti-Colic Bottle.jpg",
+    description: "A quick guide to the best bottles that reduce gas and colic for newborns"
   }
 ];
